73fa8512139b51bca928e9e7f96b12d4bbb05f50
Karen Casey-Gover <karen.caseygover@gmail.com>
2016-12-29 21:39:59 -0400

    added mock Service and Stack Description

--- src/components/Service.js
+++ src/components/Service.js
@@ -7,7 +7,8 @@ export default class Service extends React.Component {
     console.log("this in Service:   ", this);
     return (
       <div>
-        <h1>Service Component</h1>
+        <h1>Service Name</h1>
+        <p className="service-describe">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum facilisis quis neque at hendrerit. Fusce vulputate libero non maximus posuere. Mauris sed mauris laoreet, faucibus neque a, rhoncus libero. Etiam fermentum dictum fermentum. Proin sollicitudin auctor nisi at dapibus. Suspendisse lectus erat, commodo eu blandit non, congue quis mauris. Mauris.</p>
       </div>
     );
   }
--- src/components/Stack.js
+++ src/components/Stack.js
@@ -7,10 +7,11 @@ export default class Stack extends React.Component {
 
     console.log("this in Stack:   ", this);
     return (
-      <div>
-        <h1>Stack Component</h1>
+      <section>
+        <h1>Stack Name</h1>
+        <p className="stack-describe">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum facilisis quis neque at hendrerit. Fusce vulputate libero non maximus posuere. Mauris sed mauris laoreet, faucibus neque a, rhoncus libero. Etiam fermentum dictum fermentum. Proin sollicitudin auctor nisi at dapibus. Suspendisse lectus erat, commodo eu blandit non, congue quis mauris. Mauris.</p>
         < Service />
-      </div>
+      </section>
     );
   }
 }
