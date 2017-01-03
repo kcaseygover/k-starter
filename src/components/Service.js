a121bf52c01e8fcd799de94dd0f5d7c00ba99fd5
Karen Casey-Gover <karen.caseygover@gmail.com>
2017-01-03 15:47:59 -0400

    added data object for mock data and addedplaceholder link to organization on card

--- src/components/Service.js
+++ src/components/Service.js
@@ -1,15 +1,36 @@
 import React from 'react';
+// const Service = ({ children}) => (
+//   <div
+//   style={serviceStyles}
+//   >
+//   {children}
+//   <p className="service-describe" style={serviceDescriptStyles}>
+// Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum facilisis quis neque at hendrerit. Fusce vulputate libero non maximus posuere. Mauris sed mauris laoreet, faucibus neque a, rhoncus libero. Etiam fermentum dictum fermentum. Proin sollicitudin auctor nisi at dapibus. Suspendisse lectus erat, commodo eu blandit non, congue quis mauris. Mauris.
+//   </p>
+//   </div>
+// );
+
+// Service.propTypes = {
+//   children: React.PropTypes.string.isRequired,
+// };
+
+// export default Service;
+const data = {
+  serviceName: "Service Name",
+  organizationName: "Organization Name",
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum facilisis quis neque at hendrerit. Fusce vulputate libero non maximus posuere. Mauris sed mauris laoreet, faucibus neque a, rhoncus libero. Etiam fermentum dictum fermentum. Proin sollicitudin auctor nisi at dapibus. Suspendisse lectus erat, commodo eu blandit non, congue quis mauris. Mauris.",
+  price: "$0-500/mo",
+};
 
 const serviceStyles = {
   border: '1px solid #eee',
   borderRadius: 3,
-  // fontSize: 15,
+  fontSize: 15,
   padding: '15px',
   margin: '0px 0px 10px 10px',
   float: 'left',
   maxWidth: 400,
   maxHeight: 200,
-
 };
 
 const serviceDescriptStyles = {
@@ -22,16 +43,17 @@ const serviceDescriptStyles = {
 
 export default class Service extends React.Component {
 
-
-
   render() {
 
     console.log("this in Service:   ", this);
     return (
       <div className="card" style={serviceStyles} >
-        <h2>Service Name</h2>
-        <p className="service-describe" style={serviceDescriptStyles}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum facilisis quis neque at hendrerit. Fusce vulputate libero non maximus posuere. Mauris sed mauris laoreet, faucibus neque a, rhoncus libero. Etiam fermentum dictum fermentum. Proin sollicitudin auctor nisi at dapibus. Suspendisse lectus erat, commodo eu blandit non, congue quis mauris. Mauris.</p>
+        <h2>{data.serviceName}</h2>
+        <h5>by <a href="data.organizationName">{data.organizationName}</a></h5>
+        <p className="service-describe" style={serviceDescriptStyles}>{data.description}</p>
       </div>
     );
   }
 }
+
+
--- src/components/Stack.js
+++ src/components/Stack.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import StackService from './StackService';
 
+const data = {
+  stackName: "Stack Name",
+  stackDescription: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum facilisis quis neque at hendrerit. Fusce vulputate libero non maximus posuere. Mauris sed mauris laoreet, faucibus neque a, rhoncus libero. Etiam fermentum dictum fermentum. Proin sollicitudin auctor nisi at dapibus. Suspendisse lectus erat, commodo eu blandit non, congue quis mauris. Mauris.",
+};
+
 const stackContainerStyles = {
   display: 'flex',
 };
@@ -19,8 +24,8 @@ export default class Stack extends React.Component {
     return (
       <section className="stack-container row" style={stackContainerStyles}>
         <div className="stack" style={stackStyles} >
-          <h1>Stack Name</h1>
-          <p className="stack-describe">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum facilisis quis neque at hendrerit. Fusce vulputate libero non maximus posuere. Mauris sed mauris laoreet, faucibus neque a, rhoncus libero. Etiam fermentum dictum fermentum. Proin sollicitudin auctor nisi at dapibus. Suspendisse lectus erat, commodo eu blandit non, congue quis mauris. Mauris.</p>
+          <h1>{data.stackName}</h1>
+          <p className="stack-describe">{data.stackDescription}</p>
         </div>
         < StackService />
       </section>
